0ac121de9621d44627fc22643361a862fd0de455
Josinimasango <nellyjossydumako@gmail.com>
2023-08-22 02:45:47 -0700

    attempting to write code

---
+++ Betty_would_proud.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+char *firstattempt = "Hello, I am Nelisiwe!";
+
+printf("%s\n", firstattempt);
+return (0);
+}
